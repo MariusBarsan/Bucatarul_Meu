e479a05c8ae0f96bbedcddd53f1489cee0f670c5
Marius barsan <marius.barsan1985@gmail.com>
2022-08-29 20:40:51 +0300

    fixed project path

---
+++ contact/models.py
@@ -0,0 +1,13 @@
+# from django.db import models
+#
+# class Student(models.Model):
+#     nume = models.CharField(max_length=100)
+#     prenume = models.CharField(max_length=100)
+#     email = models.EmailField(max_length=100)
+#     subiect = models.CharField(max_length=150)
+#     descriere = models.TextField(max_length=300)
+#
+#
+#     #
+#     # def __str__(self):
+#     #     return f"{self.nume} {self.prenume}"
---
+++ contact/views.py
@@ -0,0 +1,9 @@
+#
+#
+#
+# class ContactFormView():
+#     template_name = 'contact/contact.html'
+#     model = ''
+#     form_class = ''
+#     success_url = reverse_lazy('')
+#     permission_required = ''
