89f4d63adf127606bd46650d0e637b0a23ca992e
Vestemean Radu <vestemean.radu@gmail.com>
2022-08-29 20:16:15 +0300

    First commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('specific',views.specific,name='specific'),
+    path('singleMeal',views.singleMeal,name="singleMeal"),
+]
+
+
---
+++ blog/views.py
@@ -0,0 +1,21 @@
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def index(request):
+    response = requests.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    jsonResponse = response.json()
+    meals = jsonResponse['meals']
+    return render(request,'blog/index.html',{'meals':meals})
+
+def specific(request):
+    return HttpResponse("This is the specific url")
+
+def singleMeal(request):
+    mealId = request.GET.get('mealId')
+    response = requests.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+str(mealId))
+    jsonResponse = response.json()
+    meals = jsonResponse['meals']
+    return render(request,'blog/single_meal.html',{'meals':meals})
