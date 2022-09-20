304418611fe6155175e9cb0a0e9f6f416a4f9f88
Marius barsan <marius.barsan1985@gmail.com>
2022-09-20 19:14:07 +0300

    deploy

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +153,6 @@ EMAIL_HOST_USER = 'ro28@horiascurtu.ro'
 EMAIL_HOST_PASSWORD = 'Django2022'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True # TLS- Transport Layer Security- protocoale criptografice care va permit sa aveti comunicatii sigure
+
+
+django_heroku.settings(locals())
